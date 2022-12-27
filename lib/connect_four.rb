4e015ad145d3709beb6b1d265b233cfa0bf81c75
jjavier <razornice@gmail.com>
2022-12-27 11:09:10 -0300

    basic class methods

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,18 +1,19 @@
 class Board
 
-  attr_reader :board, :player_a, :player_b, :player
-  attr_writer :player, :turns
+  attr_reader :board, :player_w, :player_b
+  attr_writer :turns
+  attr_accessor :played_boxes, :player
   def initialize
     @board = [
       %w[01 02 03 04 05 06 07], 
       %w[08 09 10 11 12 13 14],
       %w[15 16 17 18 19 20 21], 
-      %w[21 22 23 24 25 26 27], 
-      %w[28 29 30 31 32 33 34], 
-      %w[35 36 37 38 39 40 41]
+      %w[22 23 24 25 26 27 28], 
+      %w[29 30 31 32 33 34 35], 
+      %w[36 37 38 39 40 41 42]
     ]
     @player_w = "\u26AA".unicode_normalize!
-    @player_b = "\u25AB".unicode_normalize!
+    @player_b = "\u26D4".unicode_normalize!
     @player = player_w
     @turns = 0
     @played_boxes = Array.new
@@ -25,7 +26,36 @@ class Board
     end
    end
 
+  def cell_select
+    puts "sellect a cell between 1 a 42"
+    i = gets.chomp.to_i
+    i.between?(1, 42) ? i : cell_select
+    self.valid_move?(i) ? i : cell_select
+  end
+
+  def valid_move?(i)
+    return true unless @played_boxes.include?(i)
+    return false
+  end
+
+  def make_move(i)
+    board[0][i-1] = player if i.between?(1, 7)
+    board[1][i-8] = player if i.between?(8, 14)
+    board[2][i-15] = player if i.between?(15, 21)
+    board[3][i-22] = player if i.between?(22, 28)
+    board[4][i-29] = player if i.between?(29, 35)
+    board[5][i-36] = player if i.between?(36, 42)
+  end
+
+  def switch_player
+    if self.player == player_w
+      self.player = player_b
+    else self.player = player_w
+    end
+  end
+
 end
 
-board = Board.new
-board.print_board
+b = Board.new
+b.played_boxes.push(5)
+b.cell_select
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,9 +1,24 @@
 require "./lib/connect_four"
+
 describe Board do
-  describe "#print_board" do
-    it "prints the layout board game" do
+
+  # describe "#cell_select" do
+  #   xit "return an integer between 0 and 43" do
+  #   board = Board.new
+  #   expect(board.cell_select).to eql(5)
+  #   end
+  # end
+
+  describe "#valid_move?" do 
+    it "returns true if arg is not included in played moves" do
       board = Board.new
-      expect(board.print_board).to eql()
+      expect(board.valid_move?(5)).to eql(true)
+    end
+    it "return false if arg is already in played moves" do
+      board = Board.new
+      board.played_boxes.push(1)
+      expect(board.valid_move?(1)).to eql(false)
     end
   end
+
 end
