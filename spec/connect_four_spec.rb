fda627a4339ee5221f24b0760194ace2d9878d0f
jjavier <razornice@gmail.com>
2022-12-29 09:51:06 -0300

    spec

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -2,12 +2,12 @@ require "./lib/connect_four"
 
 describe Board do
 
-  # describe "#cell_select" do
-  #   xit "return an integer between 0 and 43" do
-  #   board = Board.new
-  #   expect(board.cell_select).to eql(5)
-  #   end
-  # end
+  describe "#cell_select" do
+    it "return an integer between 0 and 42" do
+    board = Board.new
+    expect(board.cell_select).to eql(5)
+    end
+  end
 
   describe "#valid_move?" do 
     it "returns true if arg is not included in played moves" do
@@ -20,5 +20,23 @@ describe Board do
       expect(board.valid_move?(1)).to eql(false)
     end
   end
+
+  describe "#line_four?"do
+    it "returns false if there is not a line of the same tokken" do
+      board = Board.new
+      expect(board.line_four?).to eql(false)
+    end
+
+    it "returns true if there is a line of the same tokken" do
+      board = Board.new
+      board.board[2][1] = board.player_b
+      board.board[3][1] = board.player_b
+      board.board[4][1] = board.player_b
+      board.board[5][1] = board.player_b
+      expect(board.line_four?).to eql(true)
+    end
+  end
+
+  
 
 end
